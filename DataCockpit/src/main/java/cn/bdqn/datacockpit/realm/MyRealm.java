6c89525668013734df2fbb4b353ec6cb69e1b331
Administrator <Administrator@DOJOHYCK00TQRQX.lan>
2018-01-06 08:37:51 +0800

    优化自定义realm

--- DataCockpit/src/main/java/cn/bdqn/datacockpit/realm/MyRealm.java
+++ DataCockpit/src/main/java/cn/bdqn/datacockpit/realm/MyRealm.java
@@ -1,6 +1,5 @@
 package cn.bdqn.datacockpit.realm;
 
-
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
@@ -14,42 +13,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import cn.bdqn.datacockpit.entity.Userinfo;
 import cn.bdqn.datacockpit.service.UserinfoService;
 
+public class MyRealm extends AuthorizingRealm {
 
+    @Autowired
+    private UserinfoService userService;
 
-public class MyRealm extends AuthorizingRealm{
+    /**
+     * 为当前登录的用户授予角色和权
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        String phone = (String) principals.getPrimaryPrincipal();
+        SimpleAuthorizationInfo authorizationInfo = new SimpleAuthorizationInfo();
+        authorizationInfo.setRoles(userService.getRoles(phone));
+        authorizationInfo.setStringPermissions(userService.getPermissions(phone));
+        return authorizationInfo;
+    }
 
-	@Autowired
-	private UserinfoService userService;
-	
-	/**
-	 * 为当前登录的用户授予角色和权
-	 */
-	@Override
-	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
-		String phone=(String)principals.getPrimaryPrincipal();
-		SimpleAuthorizationInfo authorizationInfo=new SimpleAuthorizationInfo();
-		authorizationInfo.setRoles(userService.getRoles(phone));
-		authorizationInfo.setStringPermissions(userService.getPermissions(phone));
-		return authorizationInfo;
-	}
+    /**
+     * 验证当前登录的用户
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        String phone = (String) token.getPrincipal();
 
-	/**
-	 * 验证当前登录的用户
-	 */
-	@Override
-	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
-		String phone=(String)token.getPrincipal();
-			
-			try {
-			    Userinfo user=userService.getByPhone(phone);
-                AuthenticationInfo authcInfo=new SimpleAuthenticationInfo(user.getPhone(),user.getPassword(),"xx");
-                return authcInfo;
-            } catch (Exception e) {
-               
-                e.printStackTrace();
-                
-            }
-            return null;
-	}
+        Userinfo user = userService.getByPhone(phone);
+        if (user != null) {
+            AuthenticationInfo authcInfo = new SimpleAuthenticationInfo(user.getPhone(), user.getPassword(), "xx");
+            return authcInfo;
+        }
+        return null;
+    }
 
 }
